d6b3556792f423987cb0ccf4d892ffcef8aaea76
mickgl <74307087+mickgl@users.noreply.github.com>
2021-04-08 14:41:56 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '1.0'
+DESCRIPTION = "Scan connected IP's using GreyNoise API"
+LONG_DESCRIPTION = "Scan connected IP's using GreyNoise API"
+
+# Setting up
+setup(
+    name="ip-checker",
+    version=VERSION,
+    author="michgl",
+    author_email="<michgl33s@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    entry_points="""
+    [console_scripts]
+    ipchecker = ipchecker.ipchecker:main
+    """,
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
